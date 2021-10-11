a90ef211f96299889ab0217dc10431677bd41769
Professional-Miles <mileslowe@pm.me>
2021-10-12 00:44:32 +0100

    User input method seperated from main body and added to it's own class.

--- src/main/java/com/sparta/simulator/app/SimulatorDriver.java
+++ src/main/java/com/sparta/simulator/app/SimulatorDriver.java
@@ -1,12 +1,9 @@
 package com.sparta.simulator.app;
 
-import com.sparta.simulator.view.DisplayManager;
-import org.apache.log4j.Level;
+import com.sparta.simulator.view.UserInput;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
-import static java.lang.System.in;
+
 
 public class SimulatorDriver {
 
@@ -14,24 +11,10 @@ public class SimulatorDriver {
 
     public static void main(String[] args) {
         PropertyConfigurator.configure("log4j.properties");
-        boolean status = true;
+        UserInput.getMonths();
 
-        while (status) {
-            System.out.print(DisplayManager.getDisplaySimTime());
-            try{
-                Scanner scanner = new Scanner(in);
-                int num = scanner.nextInt();
-                if (num >= 1) {
-                    System.out.println("\nSimulation will run for " + num + " months.");
-                    status = false;
-                } else {
-                    System.out.println("\n\tPlease enter a value larger than 0...\n");
-                }
-            } catch (NumberFormatException | NoSuchElementException e) {
-                System.out.println("\n");
-                log.log(Level.ERROR, "\n[" + e.getClass().getName() + " thrown.]\n");
-            }
-        }
+
+
     }
 }
 
---
+++ src/main/java/com/sparta/simulator/view/UserInput.java
@@ -0,0 +1,34 @@
+package com.sparta.simulator.view;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+import static java.lang.System.in;
+
+public class UserInput {
+
+    private static final Logger log = Logger.getLogger("User input logger.");
+
+    public static void getMonths(){
+
+        boolean status = true;
+        while (status) {
+            System.out.print(DisplayManager.getDisplaySimTime());
+            try{
+                Scanner scanner = new Scanner(in);
+                int num = scanner.nextInt();
+                if (num >= 1) {
+                    System.out.println("\nSimulation will run for " + num + " months.");
+                    status = false;
+                } else {
+                    System.out.println("\n\tPlease enter a value larger than 0...\n");
+                }
+            } catch (NumberFormatException | NoSuchElementException e) {
+                System.out.println("\n");
+                log.log(Level.ERROR, "\n[" + e.getClass().getName() + " thrown.]\n");
+            }
+        }
+    }
+
+}
