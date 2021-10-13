40efe577ab3dd7ae8a41c20fbe27ca7d2696ab9f
unknown <lpuma@spartaglobal.com>
2021-10-13 15:55:05 +0100

    defined Centre

---
+++ src/main/java/com/sparta/simulator/model/Centre.java
@@ -0,0 +1,10 @@
+package com.sparta.simulator.model;
+
+public abstract class Centre {
+    public boolean isFull(int numberOfTrainees){
+        return false;
+    }
+    public boolean lowCapacity(int numberOfTrainees){
+        return false;
+    }
+}
