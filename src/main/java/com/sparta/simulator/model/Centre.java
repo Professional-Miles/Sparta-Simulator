c6a5f36b18d9c592f684ae4970032c9ef07a0b51
unknown <lpuma@spartaglobal.com>
2021-10-13 15:58:52 +0100

    implement interface in centre class

--- src/main/java/com/sparta/simulator/model/Centre.java
+++ src/main/java/com/sparta/simulator/model/Centre.java
@@ -1,6 +1,6 @@
 package com.sparta.simulator.model;
 
-public abstract class Centre {
+public abstract class Centre implements CentreOperations, TraineeOperation {
     public boolean isFull(int numberOfTrainees){
         return false;
     }
