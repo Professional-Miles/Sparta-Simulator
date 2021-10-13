7b276c17951fe9032805d5673f20e3a0a28d209e
Denis-Neagu <denis.neagu@outlook.com>
2021-10-13 16:16:45 +0100

    defined Bootcamp and Tech centres

---
+++ src/main/java/com/sparta/simulator/model/BootcampCentre.java
@@ -0,0 +1,45 @@
+package com.sparta.simulator.model;
+
+import java.util.List;
+
+public class BootcampCentre extends Centre {
+    @Override
+    public int intGetCentreCapacity() {
+        return 0;
+    }
+
+    @Override
+    public void setCentreFull(boolean centreFull) {
+
+    }
+
+    @Override
+    public void addCentreCount(int centreCount) {
+
+    }
+
+    @Override
+    public int getCentreCount() {
+        return 0;
+    }
+
+    @Override
+    public boolean isCentreCountFull() {
+        return false;
+    }
+
+    @Override
+    public List<Trainee> getTrainees() {
+        return null;
+    }
+
+    @Override
+    public void setTrainees(List<Trainee> trainees) {
+
+    }
+
+    @Override
+    public void addTrainees(Trainee trainee, List<Trainee> trainees) {
+
+    }
+}
