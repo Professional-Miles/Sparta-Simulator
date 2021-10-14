119a4c37788ef5586415def4bd1405fdd17e66bb
Denis-Neagu <denis.neagu@outlook.com>
2021-10-14 15:09:19 +0100

    refactored testing for RandomGeneratorClass and added testing directory in the right place

--- src/test/java/RandomGeneratorTest.java
+++ src/test/java/RandomGeneratorTest.java
@@ -1,13 +1,12 @@
-package com.sparta.simulator.test;
-
 import com.sparta.simulator.model.RandomGenerator;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class testClass {
+
+public class RandomGeneratorTest {
 
     @Test
-    public void testRand(){
+    public void getRandomNumOfTrainees_ReturnsBetween0And50(){
         int rand = RandomGenerator.getRandomNumOfTrainees();
         assertTrue(rand>0 && rand <=50);
     }
