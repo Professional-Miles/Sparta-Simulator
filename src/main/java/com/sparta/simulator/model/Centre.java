50b68005e74d55eb326d8fb0073639f77f6ceb94
Denis-Neagu <denis.neagu@outlook.com>
2021-10-14 11:36:56 +0100

    removed redundant code

--- src/main/java/com/sparta/simulator/model/Centre.java
+++ src/main/java/com/sparta/simulator/model/Centre.java
@@ -1,20 +1,11 @@
 package com.sparta.simulator.model;
 
 public abstract class Centre implements CentreOperations, TraineeOperation {
-    public String centreType;
 
     public boolean isFull() {
         return false;
     }
     public boolean lowCapacity(int numOfTrainees) {
         return false;
-    }
-
-    public String getCentreType() {
-        return centreType;
-    }
-
-    public void setCentreType(String centreType) {
-        this.centreType = centreType;
     }
 }
