bf49c5e5997877585dd5d2eb6d6ab7be5a2ecc61
Denis-Neagu <denis.neagu@outlook.com>
2021-10-14 15:29:43 +0100

    added testing for method which returns a random centre

--- src/test/java/RandomGeneratorTest.java
+++ src/test/java/RandomGeneratorTest.java
@@ -1,4 +1,6 @@
+import com.sparta.simulator.model.Centre;
 import com.sparta.simulator.model.RandomGenerator;
+import com.sparta.simulator.model.TechCentre;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,5 +11,15 @@ public class RandomGeneratorTest {
     public void getRandomNumOfTrainees_ReturnsBetween0And50(){
         int rand = RandomGenerator.getRandomNumOfTrainees();
         assertTrue(rand>0 && rand <=50);
+    }
+
+    @Test
+    public void generateRandomCentre_ReturnsACorrectCentre() {
+        RandomGenerator randomGenerator = new RandomGenerator();
+        Centre centre = randomGenerator.generateRandomCentre();
+        Centre c = centre;
+
+        assertEquals(c,centre);
+
     }
 }
